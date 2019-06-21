7c00a7eded2b92cb83fc8b15e902c7ce39f2bfc6
Christelle <christelle@ex-algebra.com>
2019-06-21 15:47:56 +0200

    modification form oublié mot de passe
    modification form oublié mot de passe

--- wp-content/plugins/theme-my-login/templates/lostpassword-form.php
+++ wp-content/plugins/theme-my-login/templates/lostpassword-form.php
@@ -5,16 +5,15 @@ Theme My Login will always look in your theme's directory first, before using th
 */
 ?>
 <div class="tml tml-lostpassword" id="theme-my-login<?php $template->the_instance(); ?>">
-	<?php $template->the_action_template_message( 'lostpassword' ); ?>
+	<?php /*$template->the_action_template_message( 'lostpassword' );*/ ?>
+    <p class="message">Veuillez saisir votre adresse de messagerie.<br />Un lien permettant de cr&eacute;er un nouveau mot de passe vous sera envoy&eacute; par e-mail.</p>
 	<?php $template->the_errors(); ?>
 	<form name="lostpasswordform" id="lostpasswordform<?php $template->the_instance(); ?>" action="<?php $template->the_action_url( 'lostpassword', 'login_post' ); ?>" method="post">
 		<p class="tml-user-login-wrap">
-			<label for="user_login<?php $template->the_instance(); ?>"><?php
-			if ( 'email' == $theme_my_login->get_option( 'login_type' ) ) {
+			<label for="user_login<?php $template->the_instance(); ?>"><?php			
 				_e( 'E-mail:', 'theme-my-login' );
-			} else {
-				_e( 'Username or E-mail:', 'theme-my-login' );
-			} ?></label>
+				?>
+			</label>
 			<input type="text" name="user_login" id="user_login<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_login' ); ?>" size="20" />
 		</p>
 
